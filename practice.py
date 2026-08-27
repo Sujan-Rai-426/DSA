efcf1474dce24ab5de146ec22dd9558bd0132f2e
Sujan <rsujan140.in@gmail.com>
2026-08-27 13:43:02 +0545

    Bubble sort

--- practice.py
+++ practice.py
@@ -1,24 +1,21 @@
-
-# NOTE: BUBBLE Sort
-
+# Bubble sort
 # NOTE: Even if we made swap once than we need to recheck the whole array if its sorted. 
 # NOTE: And  IF while gone through whole array loop if no sawap is made than it says array is sorted
-
 def bubble_sort(array):
-    made_swapped = True  # Need at least one pass or check
+    i=0    
+    made_swap = True
     
-    while made_swapped:
-        made_swapped = False  # Reset flag
-        
-        for i in range(len(array) - 1):
-            if array[i] > array[i + 1]:
-                # Swap if bigger
-                array[i], array[i + 1] = array[i + 1], array[i]
-                made_swapped = True  # Changes made, loop again
-    
+    while made_swap:
+        made_swap = False
+        for i in range(len(array)-1):
+            if array[i] > array[i+1]:
+                array[i], array[i+1] = array[i+1], array[i]
+                made_swap = True
     return array
+        
 
 
+# Input:
 # =======> USAGE FUNCTION CALL
 raw_array_list = input("Enter list of number seperated by comma(',') : ")
 array_list = [int(item.strip()) for item in raw_array_list.split(",")]
